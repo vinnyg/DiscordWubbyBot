239f6a87a025c8477283b246c31f467726a562dc
Sam Loughlin <samloughlin2006@hotmail.co.uk>
2016-05-19 17:43:11 +0100

    Stores associations between messages and Warframe Events. As this data is now stored in Wubbybot, this class may become obsolete.

---
+++ EventsMessageAssociationManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordSharp.Objects;
+
+namespace DiscordSharpTest
+{
+    static class EventsMessageAssociationManager
+    {
+        public static Dictionary<WarframeAlert, DiscordMessage> AlertsMessagesMap { get; private set; }
+        public static Dictionary<WarframeInvasion, DiscordMessage> InvasionsMessagesMap { get; private set; }
+
+        static EventsMessageAssociationManager()
+        {
+            AlertsMessagesMap = new Dictionary<WarframeAlert, DiscordMessage>();
+            InvasionsMessagesMap = new Dictionary<WarframeInvasion, DiscordMessage>();
+        }
+
+        static void ProcessEvent(WarframeEvent targetEvent)
+        {
+            
+        }
+    };
+}
