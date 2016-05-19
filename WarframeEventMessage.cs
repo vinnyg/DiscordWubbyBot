5552d607925bde2a05abb873c0e218ce9fdf574d
Sam Loughlin <samloughlin2006@hotmail.co.uk>
2016-05-19 17:42:24 +0100

    Stores unformatted and formatted versions of the constructed string. Is likely obsolete.

---
+++ WarframeEventMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordSharpTest
+{
+    struct WarframeEventMessage
+    {
+        public string Content;
+        public string UnformattedContent;
+
+        public WarframeEventMessage(string content, string unformattedContent)
+        {
+            Content = content;
+            UnformattedContent = unformattedContent;
+        }
+    }
+}
