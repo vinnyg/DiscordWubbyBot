2a7e35d32de75cd912e6e8fc0d4fcebaaacf041e
Sam Loughlin <samloughlin2006@hotmail.co.uk>
2016-05-19 17:33:07 +0100

    A list of event argument classes.

---
+++ WEMEventEvents.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordSharpTest.Events
+{
+    class WarframeAlertScrapedArgs : EventArgs
+    {
+        /*public WarframeEventMessage Message { get { return this.Message; } private set { this.Message = value; } }*/
+        public WarframeAlert Alert { get; private set; }
+
+        public WarframeAlertScrapedArgs(WarframeAlert newAlert)
+        {
+            Alert = newAlert;
+        }
+    }
+
+    class WarframeInvasionScrapedArgs : EventArgs
+    {
+        /*public WarframeEventMessage Message { get; set; }*/
+        public WarframeInvasion Invasion { get; private set; }
+
+        public WarframeInvasionScrapedArgs(WarframeInvasion invasion)
+        {
+            //Message = new WarframeEventMessage(content, unformattedContent);
+            Invasion = invasion;
+        }
+    }
+
+    class WarframeAlertUpdatedEventArgs : EventArgs
+    {
+        public WarframeAlert Alert { get; private set; }
+
+        public WarframeAlertUpdatedEventArgs(WarframeAlert alert)
+        {
+            Alert = alert;
+        }
+    }
+}
