923ce11900c70bf840a6200d91b894b358ea246e
Sam Loughlin <samloughlin2006@hotmail.co.uk>
2016-05-19 17:40:26 +0100

    For Warframe Invasion events. Basic fields and methods provided.

---
+++ WarframeInvasion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordSharpTest
+{
+    class WarframeInvasion : WarframeEvent
+    {
+        public string Type { get; private set; }
+        public MissionInfo AttackerDetails { get; private set; }
+        public MissionInfo DefenderDetails { get; private set; }
+        private float Progress { get; set; }
+
+        public WarframeInvasion(MissionInfo attackerInfo, MissionInfo defenderInfo, string guid, string destinationName, DateTime startTime) : base(guid, destinationName, startTime)
+        {
+            //Indicates the progress made towards a particular side as a percentage.
+            Progress = .0f;
+            AttackerDetails = attackerInfo;
+            DefenderDetails = defenderInfo;
+            Type = AttackerDetails.Faction == Faction.INFESTATION ? InvasionType.OUTBREAK : InvasionType.INVASION;
+        }
+
+        public void UpdateInvasionProgress(int progress, int goal)
+        {
+            Progress = progress / goal;
+        }
+
+        override public bool HasExpired()
+        {
+            return (Progress >= 100.0f);
+        }
+    }
+}
