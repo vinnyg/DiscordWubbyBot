95909ccc8ee012ae43d887752f43f29e3f7201e2
Vincent Giang <vinnygiang@outlook.com>
2016-05-25 14:24:01 +0100

    Removed SQLite related code. SQL related operations are handled by WubbyBot. Added Start method. The class does not immediately begin parsing JSon until the Start() method is called. The Reward string is parsed outside of the MissionInfo constructor parameters to aid readability. Reward names are now parsed. New alerts are added to the NewAlerts List. Included a method to remove expired alerts from AlertsList to prevent bloating.

--- WarframeEventsContainer.cs
+++ WarframeEventsContainer.cs
@@ -7,7 +7,6 @@ using System.Xml.Linq;
 using Newtonsoft.Json.Linq;
 using System.Threading;
 using System.Net;
-using System.Data.SQLite;
 using DiscordSharpTest.Events;
 
 namespace DiscordSharpTest
@@ -17,72 +16,67 @@ namespace DiscordSharpTest
         public List<WarframeAlert> AlertsList { get; private set; }
         public List<WarframeInvasion> InvasionsList { get; private set; }
 
-        private XDocument _rssFeed { get; set; }
+        //private XDocument _rssFeed { get; set; }
         private JObject _worldState { get; set; }
 
         private Timer _eventUpdateInterval { get; set; }
 
-        private SQLiteConnection _dbConnection { get; set; }
+        private bool isRunning;
 
-        //Latest alerts and invasions from the most recent update
-        //public List<string> NewestAlerts { get; private set; }
-        //public List<string> NewestInvasions { get; private set; }
+        private WarframeDataMapper mapper;
+
+        //When an alert is read more than once, it will be added to this list. Alerts in this list are no longer new.
+        private List<WarframeAlert> NewAlerts;
 
         #region Events
         public event EventHandler<WarframeAlertScrapedArgs> AlertScraped;
         public event EventHandler<WarframeInvasionScrapedArgs> InvasionScraped;
         public event EventHandler<WarframeAlertExpiredArgs> AlertExpired;
+        public event EventHandler<ExistingAlertFoundArgs> ExistingAlertFound;
         #endregion
 
         public WarframeEventsContainer()
         {
-            //Establish event update interval (every minute)
-            _eventUpdateInterval = new Timer((e) =>
-            {
-                ScrapeWorldState();
-                ParseJsonEvents();
-            }
-            ,
-            null,
-            0,
-            (int)(TimeSpan.FromMinutes(1.0).TotalMilliseconds));
-
             AlertsList = new List<WarframeAlert>();
             InvasionsList = new List<WarframeInvasion>();
+            NewAlerts = new List<WarframeAlert>();
+            mapper = new WarframeDataMapper();
+        }
 
-            //NewestAlerts = new List<string>();
-            //NewestInvasions = new List<string>();
+        public void Start(/*Dictionary<WarframeAlert, string> alertsToCheck*/)
+        {
+            if (!isRunning)
+            {
+                //HandleOldAlerts(alertsToCheck);
+
+                //Establish event update interval (every minute)
+                _eventUpdateInterval = new Timer((e) =>
+                {
+                    ScrapeWorldState();
+                    ParseJsonEvents();
+                }, null, 0, (int)(TimeSpan.FromMinutes(1.0).TotalMilliseconds));
+
+                isRunning = true;
+            }
         }
 
         void ScrapeWorldState()
         {
             using (WebClient wc = new WebClient())
             {
-                _rssFeed = XDocument.Parse(wc.DownloadString("http://content.warframe.com/dynamic/rss.php"));
+                //_rssFeed = XDocument.Parse(wc.DownloadString("http://content.warframe.com/dynamic/rss.php"));
                 _worldState = JObject.Parse(wc.DownloadString("http://content.warframe.com/dynamic/worldState.php"));
-#if DEBUG
-                Console.WriteLine("Scraping");
-#endif
             }
         }
 
         void UpdateInvasionProgress()
         {
             throw new NotImplementedException();
-
-            /*InvasionsList.Find()
-
-            foreach (var invasion in InvasionsList)
-            {
-                invasion.UpdateInvasionProgress(_worldState["Invasions"][""])
-            }*/
         }
 
         void ParseJsonEvents()
         {
-            //Remove old entries
-            //NewestAlerts.Clear();
-            //NewestInvasions.Clear();
+            NewAlerts.Clear();
 
             //Find Alerts
             foreach (var jsonAlert in _worldState["Alerts"])
@@ -98,29 +92,34 @@ namespace DiscordSharpTest
                     JToken countables = (jsonAlert["MissionInfo"]["missionReward"]["countedItems"]), 
                         nonCountables = (jsonAlert["MissionInfo"]["missionReward"]["items"]);
 
+                    string rewardStr = (countables != null ?
+                        mapper.GetItemName(countables[0]["ItemType"].ToString()) :
+                        (nonCountables != null ? mapper.GetItemName(nonCountables[0].ToString()) : ""));
+
                     double secondsUntilStart = double.Parse(jsonAlert["Activation"]["sec"].ToString()) - double.Parse(_worldState["Time"].ToString());
                     double secondsUntilExpire = double.Parse(jsonAlert["Expiry"]["sec"].ToString()) - double.Parse(_worldState["Time"].ToString());
                     DateTime startTime = DateTime.Now.AddSeconds(secondsUntilStart);
                     DateTime expireTime = DateTime.Now.AddSeconds(secondsUntilExpire);
                     
-                    MissionInfo alertInfo = new MissionInfo(jsonAlert["MissionInfo"]["faction"].ToString(),
-                        jsonAlert["MissionInfo"]["missionType"].ToString(),
-                        int.Parse(jsonAlert["MissionInfo"]["missionReward"]["credits"].ToString()),
-                        //If for whatever reason, an alert returns both countables and non-countables, currently the countables will be returned.
-                        //In addition, if an alert returns multiple different countables, only the first instance will be returned. This affects invasions as well!
-                        (countables != null ? countables[0]["ItemType"].ToString() : (nonCountables != null ? nonCountables[0].ToString() : "")),
-                        int.Parse((countables != null ? countables[0]["ItemCount"] : 1).ToString()),
-                        int.Parse(jsonAlert["MissionInfo"]["minEnemyLevel"].ToString()),
-                        int.Parse(jsonAlert["MissionInfo"]["maxEnemyLevel"].ToString()));
+                    if (DateTime.Now < expireTime)
+                    {
+                        MissionInfo alertInfo = new MissionInfo(jsonAlert["MissionInfo"]["faction"].ToString(),
+                            jsonAlert["MissionInfo"]["missionType"].ToString(),
+                            int.Parse(jsonAlert["MissionInfo"]["missionReward"]["credits"].ToString()),
+                            //If for whatever reason, an alert returns both countables and non-countables, currently the countables will be returned.
+                            //In addition, if an alert returns multiple different countables, only the first instance will be returned. This affects invasions as well!
+                            rewardStr,
+                            int.Parse((countables != null ? countables[0]["ItemCount"] : 1).ToString()),
+                            int.Parse(jsonAlert["MissionInfo"]["minEnemyLevel"].ToString()),
+                            int.Parse(jsonAlert["MissionInfo"]["maxEnemyLevel"].ToString()));
 
-                    currentAlert = new WarframeAlert(alertInfo, id, loc, startTime, expireTime);
-                    AlertsList.Add(currentAlert);
-                    //NewestAlerts.Add(id);
-                    //CreateAlertReceivedEvent(currentAlert);
-
+                        currentAlert = new WarframeAlert(alertInfo, id, mapper.GetNodeName(loc), startTime, expireTime);
+                        AlertsList.Add(currentAlert);
+                        NewAlerts.Add(currentAlert);
 #if DEBUG
-                    Console.WriteLine("New Alert Event");
+                        Console.WriteLine("New Alert Event");
 #endif
+                    }
                 }
                 else
                 {
@@ -128,7 +127,11 @@ namespace DiscordSharpTest
                     Console.WriteLine("Update Alert Event");
 #endif
                 }
-                CreateAlertReceivedEvent(currentAlert);
+
+                if (currentAlert.ExpireTime > DateTime.Now)
+                    CreateNewAlertReceivedEvent(currentAlert);
+                else
+                    CreateAlertExpiredEvent(currentAlert, "");
             }
 
             //Find Invasions
@@ -170,6 +173,20 @@ namespace DiscordSharpTest
             }
         }
 
+        private void RemoveExpiredAlerts()
+        {
+            List<WarframeAlert> AlertsListToIterate = new List<WarframeAlert>(AlertsList);
+
+            foreach(var alert in AlertsListToIterate)
+            {
+                if (DateTime.Now > alert.ExpireTime)
+                {
+                    WarframeAlert targetAlert = AlertsList.Find(x => x.GUID == alert.GUID);
+                    AlertsList.Remove(targetAlert);
+                }
+            }
+        }
+
         public WarframeAlert GetAlert(string alertID)
         {
             return AlertsList.Find(x => x.GUID == alertID);
@@ -180,7 +197,7 @@ namespace DiscordSharpTest
             return InvasionsList.Find(x => x.GUID == invasionID);
         }
 
-        private void CreateAlertReceivedEvent(WarframeAlert newAlert)
+        private void CreateNewAlertReceivedEvent(WarframeAlert newAlert)
         {
             // Make a temporary copy of the event to avoid possibility of
             // a race condition if the last subscriber unsubscribes
@@ -193,14 +210,22 @@ namespace DiscordSharpTest
                 handler(this, e);
         }
 
-        private void CreateAlertExpiredEvent(WarframeAlert expiredAlert, string messageID)
+        private void CreateAlertExpiredEvent(WarframeAlert expiredAlert, string messageID = "")
         {
-            // Make a temporary copy of the event to avoid possibility of
-            // a race condition if the last subscriber unsubscribes
-            // immediately after the null check and before the event is raised.
             EventHandler<WarframeAlertExpiredArgs> handler = AlertExpired;
 
             WarframeAlertExpiredArgs e = new WarframeAlertExpiredArgs(expiredAlert, messageID);
+
+            if (handler != null)    //Check if there are any subscribers
+                handler(this, e);
+        }
+
+        [Obsolete]
+        private void CreateExistingAlertFoundEvent(WarframeAlert alert, string messageID = "")
+        {
+            EventHandler<ExistingAlertFoundArgs> handler = ExistingAlertFound;
+
+            ExistingAlertFoundArgs e = new ExistingAlertFoundArgs(alert, messageID);
 
             if (handler != null)    //Check if there are any subscribers
                 handler(this, e);
@@ -211,27 +236,25 @@ namespace DiscordSharpTest
             return !(AlertsList.Find(x => x.GUID == alert.GUID) == null);
         }
 
-        public void HandleExpiredAlerts(Dictionary<WarframeAlert, string> alerts)
+        [Obsolete]
+        public void HandleOldAlerts(Dictionary<WarframeAlert, string> alerts)
         {
             //Iterate through the dictionary of events and identify any alerts which have expired.
             //WubbyBot will handle accordingly.
             foreach(KeyValuePair<WarframeAlert, string> entry in alerts)
             {
-                if (entry.Key.ExpireTime > DateTime.Now)
-                {
+                Console.WriteLine("Checking for expire state");
+
+                if (entry.Key.ExpireTime < DateTime.Now)
                     CreateAlertExpiredEvent(entry.Key, entry.Value);
-                }
+                else
+                    CreateExistingAlertFoundEvent(entry.Key, entry.Value);
             }
         }
 
-        /*private void CreateAlertUpdatedEvent(WarframeAlert existingAlert)
+        public bool IsAlertNew(WarframeAlert alert)
         {
-            EventHandler<WarframeAlertScrapedArgs> handler = AlertScraped;
-
-            WarframeAlertScrapedArgs e = new WarframeAlertScrapedArgs(existingAlert);
-
-            if (handler != null)    //Check if there are any subscribers
-                handler(this, e);
-        }*/
+            return (NewAlerts.Find(x => x.GUID == alert.GUID) != null);
+        }
     }
 }
