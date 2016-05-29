5d622421c7dd0a61fe2cedea2d8364eb92d89609
Vincent Giang <vinnygiang@outlook.com>
2016-05-29 15:15:26 +0100

    Progress of invasions is now parsed correctly.
    Related Work Items: #11

--- WarframeInvasion.cs
+++ WarframeInvasion.cs
@@ -12,8 +12,9 @@ namespace DiscordSharpTest
         public MissionInfo AttackerDetails { get; private set; }
         public MissionInfo DefenderDetails { get; private set; }
         private float Progress { get; set; }
+        private int Goal { get; set; }
 
-        public WarframeInvasion(MissionInfo attackerInfo, MissionInfo defenderInfo, string guid, string destinationName, DateTime startTime) : base(guid, destinationName, startTime)
+        public WarframeInvasion(MissionInfo attackerInfo, MissionInfo defenderInfo, string guid, string destinationName, DateTime startTime, int goal) : base(guid, destinationName, startTime)
         {
             //Indicates the progress made towards a particular side as a percentage.
             Progress = .0f;
@@ -21,11 +22,13 @@ namespace DiscordSharpTest
             DefenderDetails = defenderInfo;
             //We check the defender information because the defender information contains information corresponding to the mission that they give and vice versa.
             Type = DefenderDetails.Faction == Faction.INFESTATION ? InvasionType.OUTBREAK : InvasionType.INVASION;
+            Goal = goal;
         }
 
-        public void UpdateProgress(int progress, int goal)
+        public void UpdateProgress(int progress)
         {
-            Progress = progress / goal;
+            int direction = progress != 0 ? (System.Math.Abs(progress) / progress) : 1;
+            Progress = (((float)System.Math.Abs(progress) / (float)Goal) * direction);
         }
 
         public float GetProgress()
