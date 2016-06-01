3569a42f32486d5ac61e8da4d9678dbb9b0f80af
Vincent Giang <vinnygiang@outlook.com>
2016-06-01 17:46:30 +0100

    Added to source control.

---
+++ WarframeEvent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace DiscordSharpTest
+{
+    /*----Message Example----/
+    Destination: Acanth, Eris (28-30)
+    Mission: Hive Sabotage (Infestation)
+    Reward: 1xNitain Extract, 11800cr    
+    Status: Expired 13:37
+    ------------------------*/
+
+    /*----Message Example----/
+    Destination: Acanth, Eris
+    Mission: Grineer (Extermination) vs Corpus (Spy)
+    Reward: Orokin Catalyst // Orokin Catalyst
+    Progress: 69% (Corpus)
+    ------------------------*/
+
+    /*----Message Example----/
+    Destination: Acanth, Eris
+    Mission: Infestation vs Corpus (Spy)
+    Reward: 3xFieldron
+    Progress: 69% (Corpus)
+    ------------------------*/
+
+    abstract class WarframeEvent
+    {
+        public string GUID { get; private set; }
+        public string DestinationName { get; private set; }
+        public DateTime StartTime { get; private set; }
+
+        public WarframeEvent(string guid, string destinationName, DateTime startTime)
+        {
+            GUID = guid;
+            DestinationName = destinationName;
+            StartTime = startTime;
+        }
+
+        public abstract bool IsExpired();
+
+        protected void SetDestinationNode(string destination)
+        {
+            DestinationName = destination;
+        }
+    }
+}
