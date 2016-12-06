81d644da480678350f2e7123bed4b7b322694459
Vincent Giang <vinnygiang@outlook.com>
2016-12-06 19:39:13 +0000

    Removed obsolete properties from code.

--- DiscordWrapper/Config.cs
+++ DiscordWrapper/Config.cs
@@ -9,30 +9,8 @@ namespace DiscordWrapper
 {
     public class Config
     {
-        [JsonProperty("bot_email")]
-        public string email { get; internal set; }
-
-        [JsonProperty("bot_password")]
-        public string password { get; internal set; }
-
         [JsonProperty("command_prefix")]
         public char commandPrefix { get; internal set; }
-
-        [Obsolete]
-        [JsonProperty("consumer_key")]
-        public string consumerKey { get; internal set; }
-
-        [Obsolete]
-        [JsonProperty("consumer_secret")]
-        public string consumerSecret { get; internal set; }
-
-        [Obsolete]
-        [JsonProperty("access_token")]
-        public string accessToken { get; internal set; }
-
-        [Obsolete]
-        [JsonProperty("access_token_secret")]
-        public string accessTokenSecret { get; internal set; }
 
         [JsonProperty("discord_token")]
         public string DiscordToken { get; internal set; }
