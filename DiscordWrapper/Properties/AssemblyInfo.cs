40dc0c55428c08309f0d164148ed32f70e0ed159
Vincent Giang <vinnygiang@outlook.com>
2016-12-06 11:35:46 +0000

    Added information.

--- DiscordWrapper/Properties/AssemblyInfo.cs
+++ DiscordWrapper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DiscordWrapper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Wrapper for the Discord API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DiscordWrapper")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.*")]
