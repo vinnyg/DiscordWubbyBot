195270a07ccbda9e1cd3cba706cb715b42a73489
Vincent Giang <vinnygiang@outlook.com>
2016-12-06 19:39:41 +0000

    Replaced DiscordSharp with DSharpPlus Discord API library.

--- DiscordShMain.cs
+++ DiscordShMain.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DiscordSharp;
+//using DiscordSharp;
 using System.Reflection;
 using System.IO;
 
