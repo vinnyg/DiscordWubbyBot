c16a40c19a686e767d1c56581d3d348d299c628a
Vincent Giang <vinnygiang@outlook.com>
2016-12-09 14:12:24 +0000

    Added an extension method to reverse strings.

---
+++ Events/Extensions/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordSharpTest.Events.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static string Reverse(this string str)
+        {
+            var s = str.ToCharArray();
+            Array.Reverse(s);
+            return new string(s);
+        }
+    }
+}
