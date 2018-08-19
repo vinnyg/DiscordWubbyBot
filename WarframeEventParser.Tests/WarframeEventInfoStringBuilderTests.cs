c51709edcc195c729eb28979b8032fb8f838fa6d
Vincent Giang <VinnyGiang@outlook.com>
2018-08-19 20:31:31 +0100

    refactor work to simplify classes

---
+++ WarframeEventParser.Tests/WarframeEventInfoStringBuilderTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarframeEventServices.Tests
+{
+    [TestFixture]
+    public class WarframeEventInfoStringBuilderTests
+    {
+        [Test]
+        public void Should_BuildAMessageForAlert()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ WarframeEventParser.Tests/WarframeEventParserTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WarframeEventServices.Tests
+{
+    [TestClass]
+    public class WarframeEventParserTests
+    {
+        [TestMethod]
+        public void Should_BuildAStringMessageForAnAlert()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
