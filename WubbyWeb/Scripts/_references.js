c34c451ed69ff3c4993ce047da22f1f5d662d3cb
Vincent Giang <VinnyGiang@outlook.com>
2018-08-19 13:35:10 +0100

    deleted wubbyweb project

--- WubbyWeb/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="jquery-3.1.1.js" />
-/// <autosync enabled="true" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="bootstrap.js" />
-/// <reference path="respond.js" />
